17e58b1ab31db5cb3e97526c26b27ebed7099157
Erik Huang <ekhu94@gmail.com>
2021-06-22 16:58:29 -0700

    create TextBox class with starter methods

---
+++ src/com/ekhu94/TextBox.java
@@ -0,0 +1,17 @@
+package com.ekhu94;
+
+public class TextBox {
+    private String text;
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        this.text = "";
+    }
+}
