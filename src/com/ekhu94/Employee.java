9c4ec3814ec85f4923dd34573b709468452d6863
Erik Huang <ekhu94@gmail.com>
2021-06-22 17:41:36 -0700

    setters validations

--- src/com/ekhu94/Employee.java
+++ src/com/ekhu94/Employee.java
@@ -14,6 +14,9 @@ public class Employee {
     }
 
     public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0) {
+            throw new IllegalArgumentException("Salary must be greater than 0");
+        }
         this.baseSalary = baseSalary;
     }
 
@@ -22,6 +25,9 @@ public class Employee {
     }
 
     public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0) {
+            throw new IllegalArgumentException("Hourly rate must be greater than 0");
+        }
         this.hourlyRate = hourlyRate;
     }
 
