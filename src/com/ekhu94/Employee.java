6f9d34aaf063f5052d33d96ccdfd04e1c7f82b11
Erik Huang <ekhu94@gmail.com>
2021-06-22 17:33:20 -0700

    generate getters/setters

--- src/com/ekhu94/Employee.java
+++ src/com/ekhu94/Employee.java
@@ -9,6 +9,22 @@ public class Employee {
         this.hourlyRate = hourlyRate;
     }
 
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
     }
